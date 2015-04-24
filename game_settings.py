c2fb76c6f999de33d901144809499e4f6e9c88b9
Jakob Groß <jakobgross.gross@gmail.com>
2015-04-24 15:09:29 +0200

    game_settings.py
    Konstanten

---
+++ game_settings.py
@@ -0,0 +1,16 @@
+__author__ = 'JG'
+
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+BLUE     = (   0,   0, 255)
+PINK     = ( 255,   0, 255)
+
+COLORCODE= PINK                  # ACHTUNG DIES IST DIE COLORCODING FARBE FÜR DIE SPRITESHEETS
+BGCOLOR = BLACK                  # Hintergrundfarbe
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+
+SPRITEX = 50
+SPRITEY = 50
+
+FPS=30
